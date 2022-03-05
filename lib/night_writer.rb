b5340aa54bc7a337f3d3db583dd989c071e530d3
Adrian Campos <adriancampos2992@yahoo.com>
2022-03-05 14:35:41 -0800

    Feat: add number of character to print out to command line

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -5,9 +5,13 @@ incoming_text = handle.read
 handle.close
 
 cap_incoming_text = incoming_text.upcase
+count_incoming_text = cap_incoming_text.length
 
 writer = File.open(ARGV[1], "w")
+
 
 writer.write(cap_incoming_text)
 
 writer.close
+
+puts "Created #{ARGV[1]} containing #{count_incoming_text} characters"
