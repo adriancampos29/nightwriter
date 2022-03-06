8b5cdfed1f6ecc16aab661026d340def4e0e24f5
Adrian Campos <adriancampos2992@yahoo.com>
2022-03-05 16:30:33 -0800

    Feat: made alphabest hash with keys as letters and value as the braille symbols

---
+++ lib/braille_translator.rb
@@ -0,0 +1,8 @@
+# this method will handle everything, to translate, output to the night_writer.rb
+
+# output needs to string of braille, with line breaks inbetween
+
+# convert english to braille coordinates, manipulate nested arrays or arrays to get format we want
+
+# build methods for single letter, than build method for word, for 40/more characters build another method 
+p alphabet_hash[:a]
