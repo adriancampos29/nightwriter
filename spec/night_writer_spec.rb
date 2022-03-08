145b9a590587219101a3c13b1edc019f959c7c72
Adrian Campos <adriancampos2992@yahoo.com>
2022-03-07 16:29:55 -0800

    Test: add given multiple letters, output is converted to braille

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -7,7 +7,17 @@ require './lib/night_writer'
     message = "h"
     writer = NightWriter.new(message)
     expect(writer).to be_an_instance_of(NightWriter)
-    expect(writer.braille).to be_a(String)
-    expect(writer.braille).to eq("O . O O . .")
+    expect(writer.braille).to be_a(Array)
+    expect(writer.braille).to eq(["O . O O . ."])
   end
+
+  it 'given hello world, returns braille equivalent' do
+    message = "hello world"
+    writer = NightWriter.new(message)
+    expected = ["O . O O . .","O . . O . .","O . O . O .", "O . O . O .", "O . . O O .", ". O O O . O","O . . O O .","O . O O O .","O . O . O .","O O . O . ."]
+    expect(writer).to be_an_instance_of(NightWriter)
+    expect(writer.braille).to be_a(Array)
+    expect(writer.braille).to eq(expected)
+  end
+
 end
